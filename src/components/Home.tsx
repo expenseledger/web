81240e003e3c49f007940aefff063cc14a67b8df
oat9002 <oat9002@hotmail.com>
2018-12-09 12:19:43 +0700

    Add buttton to add transaction

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,15 @@
-import 'bulma/css/bulma.css';
-import * as React from 'react';
-import * as CategoryService from '../service/CategoryService';
+import "bulma/css/bulma.css";
+import * as React from "react";
+import * as CategoryService from "../service/CategoryService";
+import * as TransactionService from "../service/TransactionService";
+import AddTransactionRequest from "src/service/Model/Request/AddTransactionRequest";
 
 class Home extends React.Component {
   public state = {
-    categories: []
+    categories: [],
   }
+
+  public AddTransactionRequest = new AddTransactionRequest()
 
   public async componentDidMount() {
     const categories = await CategoryService.getAllCategories();
@@ -36,28 +40,48 @@ class Home extends React.Component {
     return toReturn;
   }
 
+  public inputHandler = (event: any) => {
+    this.AddTransactionRequest[event.target.name] = event.target.value;
+  }
+
+  public addTransactionHandler = async () => {
+    const response = await TransactionService.addTransaction(this.AddTransactionRequest);
+
+    if(response) {
+      alert('Add transaction success');
+    }
+    else {
+      alert('Add transaction failed');
+    }
+  }
+
   public render() {
     return (
-      <div className='container is-fluid'>
-        <p style={{textAlign: 'center'}}>Add Transaction</p>
-        <p style={{textAlign: 'center'}}>Category</p>
-        <div className='container has-text-centered'>
+      <div className="container is-fluid">
+        <p style={{textAlign: "center"}}>Add Transaction</p>
+        <p style={{textAlign: "center"}}>Category</p>
+        <div className="container has-text-centered">
           { this.renderCategory() }
-          <div className='columns'>
-            <div className='column'>
+          <div className="columns">
+            <div className="column">
               <div className="field">
                 <div className="control">
-                  <input className="input is-rounded" type="text" placeholder="Amount"/>
+                  <input className="input is-rounded" name="amount" type="text" placeholder="Amount" onChange={ this.inputHandler }/>
                 </div>
               </div>
               <div className="field">
                 <div className="control">
-                  <input className="input is-rounded" type="date"/>
+                  <input className="input is-rounded" name="date" type="date" onChange={ this.inputHandler }/>
                 </div>
               </div>
               <div className="field">
                 <div className="control">
-                  <textarea className="textarea" placeholder="Note"/>
+                  <textarea className="textarea" name="note" placeholder="Note" onChange={ this.inputHandler }/>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <button className="button is-primary" onClick={ this.addTransactionHandler }>Add</button>
                 </div>
               </div>
             </div> 

ce4ed8b64652a4f70b309107d8333d959163f77c
oat9002 <oat9002@hotmail.com>
2019-07-21 15:11:17 +0700

    Enhance scss

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -2,18 +2,17 @@ import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../components/Home";
 import More from "../components/More";
-import "./App.scss";
 import { TransactionList } from "./TransactionList";
 
 class App extends React.Component {
-  public render() {
-    return (
-      <Router>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/transactionList/:walletName" exact={true} component={TransactionList} />
-        <Route path="/more" exact={true} component={More} />
-      </Router>);
-  }
+    public render() {
+        return (
+            <Router>
+                <Route path="/" exact={true} component={Home} />
+                <Route path="/transactionList/:walletName" exact={true} component={TransactionList} />
+                <Route path="/more" exact={true} component={More} />
+            </Router>);
+    }
 }
 
 export default App;

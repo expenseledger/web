b5c81cd96824ffacc91fd2488da3782a2e068356
oat9002 <oat9002@hotmail.com>
2019-08-04 13:11:07 +0700

    Align more page

--- src/components/More.tsx
+++ src/components/More.tsx
@@ -47,47 +47,88 @@ class More extends React.Component<RouteComponentProps, MoreState> {
                     {this.renderTransactoinTypeTab()}
                 </div>
                 <div className="more__content__category">
-                    <span className="more__content__category__title">Category </span>
+                    <span className="more__content__category__title">
+                        Category{" "}
+                    </span>
                     <Dropdown
                         className="more__content__category__dropdown"
-                        default={this.state.currentValue.category && this.state.currentValue.category.name}
+                        default={
+                            this.state.currentValue.category &&
+                            this.state.currentValue.category.name
+                        }
                         options={this.state.categories.map(x => x.name)}
                         updateSelectedValue={this.updateSelectedCategory}
                     />
                 </div>
                 <div className="more__content__transaction">
                     <div className="more__content__transaction__from">
-                        <span className="more__content__transaction__from__title">From </span>
+                        <span className="more__content__transaction__from__title">
+                            From{" "}
+                        </span>
                         <Dropdown
                             className="more__content__transaction__from__dropdown"
-                            default={this.state.currentValue.fromWallet && this.state.currentValue.fromWallet.name}
+                            default={
+                                this.state.currentValue.fromWallet &&
+                                this.state.currentValue.fromWallet.name
+                            }
                             options={this.state.wallets.map(x => x.name)}
                             updateSelectedValue={this.updateSelectedFromWallet}
                         />
-                        <span className="more__content__transaction__from__balance"> {this.state.currentValue.fromWallet ? this.state.currentValue.fromWallet.balance : 0} THB</span>
+                        <span className="more__content__transaction__from__balance">
+                            {" "}
+                            {this.state.currentValue.fromWallet
+                                ? this.state.currentValue.fromWallet.balance
+                                : 0}{" "}
+                            THB
+                        </span>
                     </div>
                     <div className="more__content__transaction__to">
-                        <span className="more__content__transaction__to__title">To </span>
+                        <span className="more__content__transaction__to__title">
+                            To{" "}
+                        </span>
                         <Dropdown
                             className="more__content__transaction__to__dropdown"
-                            default={this.state.currentValue.toWallet && this.state.currentValue.toWallet.name}
+                            default={
+                                this.state.currentValue.toWallet &&
+                                this.state.currentValue.toWallet.name
+                            }
                             options={this.state.wallets.map(x => x.name)}
                             updateSelectedValue={this.updateSelectedToWallet}
                         />
-                        <span className="more__content__transaction__to__balance"> {this.state.currentValue.toWallet ? this.state.currentValue.toWallet.balance : 0} THB </span>
+                        <span className="more__content__transaction__to__balance">
+                            {" "}
+                            {this.state.currentValue.toWallet
+                                ? this.state.currentValue.toWallet.balance
+                                : 0}{" "}
+                            THB{" "}
+                        </span>
                     </div>
                 </div>
                 <div className="more__content__date">
                     <span className="more__content__date__title">Date</span>
-                    <DateBox className="more__content__date__box" name="date" updateValue={this.updateSelectedDate} />
+                    <DateBox
+                        className="more__content__date__box"
+                        name="date"
+                        updateValue={this.updateSelectedDate}
+                    />
                 </div>
                 <div className="more__content__amount">
                     <span className="more__content__amount__title">Amount</span>
-                    <TextBox className="more__content__amount__box" updateValue={this.updateExpense} name="expnese" />
+                    <TextBox
+                        className="more__content__amount__box"
+                        updateValue={this.updateExpense}
+                        name="expnese"
+                    />
                 </div>
                 <div className="more__content__description">
-                    <span className="more__content__description__title">Description</span>
-                    <TextField className="more__content__description__box" name="description" updateValue={this.updateDescription} />
+                    <span className="more__content__description__title">
+                        Description
+                    </span>
+                    <TextField
+                        className="more__content__description__box"
+                        name="description"
+                        updateValue={this.updateDescription}
+                    />
                 </div>
                 <div>
                     <Button value="Add" />
@@ -101,37 +142,58 @@ class More extends React.Component<RouteComponentProps, MoreState> {
 
         return (
             <ul>
-                {transactionTypes.map((t, idx) => <li className={this.state.transactionTypeTabActive[idx] ? "is-active" : ""} key={t} data-id={idx} onClick={this.transactionTypeTabOnClickHandler}><a>{t}</a></li>)}
+                {transactionTypes.map((t, idx) => (
+                    <li
+                        className={
+                            this.state.transactionTypeTabActive[idx]
+                                ? "is-active"
+                                : ""
+                        }
+                        key={t}
+                        data-id={idx}
+                        onClick={this.transactionTypeTabOnClickHandler}
+                    >
+                        <a>{t}</a>
+                    </li>
+                ))}
             </ul>
         );
     }
 
     private transactionTypeTabOnClickHandler = (e: any) => {
         const currentTabActive = [...this.state.transactionTypeTabActive];
-        const tabActive = currentTabActive.map((t, idx) => idx === Number.parseInt(e.currentTarget.dataset.id, 10) ? true : false);
+        const tabActive = currentTabActive.map((t, idx) =>
+            idx === Number.parseInt(e.currentTarget.dataset.id, 10)
+                ? true
+                : false
+        );
 
         this.setState({
-            transactionTypeTabActive: tabActive,
+            transactionTypeTabActive: tabActive
         });
-    }
+    };
 
     private updateSelectedCategory = (value: string) => {
         const currentValue = { ...this.state.currentValue };
 
-        currentValue.category = this.state.categories.find(x => x.name === value);
+        currentValue.category = this.state.categories.find(
+            x => x.name === value
+        );
         this.setState({
             currentValue
         });
-    }
+    };
 
     private updateSelectedFromWallet = (value: string) => {
         const currentValue = { ...this.state.currentValue };
 
-        currentValue.fromWallet = this.state.wallets.find(x => x.name === value);
+        currentValue.fromWallet = this.state.wallets.find(
+            x => x.name === value
+        );
         this.setState({
             currentValue
         });
-    }
+    };
 
     private updateSelectedToWallet = (value: string) => {
         const currentValue = { ...this.state.currentValue };
@@ -140,7 +202,7 @@ class More extends React.Component<RouteComponentProps, MoreState> {
         this.setState({
             currentValue
         });
-    }
+    };
 
     private updateSelectedDate = (value: string) => {
         const currentValue = { ...this.state.currentValue };
@@ -149,7 +211,7 @@ class More extends React.Component<RouteComponentProps, MoreState> {
         this.setState({
             currentValue
         });
-    }
+    };
 
     private updateExpense = (value: string) => {
         const currentValue = { ...this.state.currentValue };
@@ -158,7 +220,7 @@ class More extends React.Component<RouteComponentProps, MoreState> {
         this.setState({
             currentValue
         });
-    }
+    };
 
     private updateDescription = (value: string) => {
         const currentValue = { ...this.state.currentValue };
@@ -167,7 +229,7 @@ class More extends React.Component<RouteComponentProps, MoreState> {
         this.setState({
             currentValue
         });
-    }
+    };
 
     private initializeState(homeState: HomeState): MoreState {
         return {
@@ -179,7 +241,7 @@ class More extends React.Component<RouteComponentProps, MoreState> {
                 category: homeState.currentValue.category,
                 amount: homeState.currentValue.amount,
                 date: homeState.currentValue.date,
-                description: "",
+                description: ""
             },
             transactionTypeTabActive: [false, false, false]
         };

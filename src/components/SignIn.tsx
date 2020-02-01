3e0843d132923ddc5d31a35e34c3c6b052a32841
Kohpai <whcwhc78@gmail.com>
2020-02-01 16:49:24 +0700

    :art: format indentation

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -7,37 +7,36 @@ import Logo from "../assets/pics/logo.svg";
 import { withRouter, RouteComponentProps } from "react-router";
 
 function SignIn(props: RouteComponentProps) {
-    React.useEffect(() => {
-        const unregisterAuthObserver = firebase
-            .auth()
-            .onAuthStateChanged(user =>
-                !!user ? props.history.replace("/") : null
-            );
+  React.useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(user => (!!user ? props.history.replace("/") : null));
 
-        return () => unregisterAuthObserver();
-    }, []);
+    return () => unregisterAuthObserver();
+  }, []);
 
-    const uiConfig: firebaseui.auth.Config = {
-        signInFlow: "popup",
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        ],
-        callbacks: {
-            signInSuccessWithAuthResult: () => false
-        }
-    };
+  const uiConfig: firebaseui.auth.Config = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: authResult => {
+        console.log("Kohpai Jaa", authResult);
+        return false;
+      }
+    }
+  };
 
-    return (
-        <div className="signIn">
-            <img className="siginIn__logo" src={Logo} />
-            <span className="signIn__title">Welcome to Expense Ledger</span>
-            <StyledFirebaseAuth
-                uiConfig={uiConfig}
-                firebaseAuth={firebase.auth()}
-            />
-        </div>
-    );
+  return (
+    <div className="signIn">
+      <img className="siginIn__logo" src={Logo} />
+      <span className="signIn__title">Welcome to Expense Ledger</span>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
 }
 
 export default withRouter(SignIn);

97ad1380435021ae7ff34fff4c26a0d8e69b39ab
Kohpai <whcwhc78@gmail.com>
2020-02-01 16:50:58 +0700

    :fire: delete FB auth option

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -19,8 +19,7 @@ function SignIn(props: RouteComponentProps) {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       signInSuccessWithAuthResult: authResult => {

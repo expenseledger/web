f8e004377082ff1636fe0f397cc641c27787c93f
oat9002 <oat9002@hotmail.com>
2020-09-16 22:08:54 +0700

    :recycle: Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -51,7 +51,7 @@ const categoriesState = atom<Category[]>({
 function Home2(props: RouteComponentProps) {
     const [wallets, setWallets] = useRecoilState(walletsState);
     const [categories, setCategories] = useRecoilState(categoriesState);
-    const [currentValue, setCurrentValue] = React.useState({
+    const [currentValue, setCurrentValue] = React.useState<CurrentValue>({
         wallet: {
             name: "",
             balance: 0,
@@ -65,7 +65,9 @@ function Home2(props: RouteComponentProps) {
     });
     const [isLoading, setIsLoading] = React.useState(true);
     const [isShowAddCategory, setIsShowAddCategory] = React.useState(true);
-    const [notificationList, setNotificationList] = React.useState([]);
+    const [notificationList, setNotificationList] = React.useState<
+        NotificationProps[]
+    >([]);
     const fetchAllWallet = React.useCallback(async () => {
         const tWallets = await WalletService.getAllWallet();
         const tCurrentValue = R.clone(currentValue);
@@ -95,6 +97,114 @@ function Home2(props: RouteComponentProps) {
         });
     }, [fetchAllCategory, fetchAllWallet]);
 
+    const renderAddCategory = () => {
+        return isShowAddCategory ? (
+            <TextBoxWithButton onClick={addCategory} />
+        ) : null;
+    };
+
+    const updateSelectedDate = (value: string) => {
+        const tCurrentValue = R.clone(currentValue);
+
+        tCurrentValue.date = value;
+        setCurrentValue(tCurrentValue);
+    };
+
+    const updateSelectedWallet = (value: string) => {
+        const tCurrentValue = R.clone(currentValue);
+
+        tCurrentValue.wallet = wallets.find((x) => x.name === value);
+        setCurrentValue(tCurrentValue);
+    };
+
+    const updateSelectedCategory = (value: string) => {
+        const tCurrentValue = R.clone(currentValue);
+
+        tCurrentValue.category = categories.find((x) => x.name === value);
+        setCurrentValue(tCurrentValue);
+    };
+
+    const updateExpense = (value: string) => {
+        const tCurrentValue = R.clone(currentValue);
+
+        tCurrentValue.amount = Number.parseFloat(value);
+        setCurrentValue(tCurrentValue);
+    };
+
+    const addTransaction = async () => {
+        const { wallet, amount, date, category } = currentValue;
+        const request: AddExpenseRequest = {
+            amount,
+            category: category ? category.name : "",
+            from: wallet ? wallet.name : "",
+            description: "quick add appense",
+            date,
+        };
+
+        const response = await addExpense(request);
+
+        if (response) {
+            const tWallets = R.clone(wallets);
+            const selectedWalletIdx = tWallets.findIndex(
+                (x) => x.name === response.srcWallet.name
+            );
+
+            tWallets[selectedWalletIdx].balance = response.srcWallet.balance;
+            setWallets(tWallets);
+            setNotificationList((prevNotiList) =>
+                prevNotiList.concat(
+                    mapNotificationProps("AddExpense sucess", "success")
+                )
+            );
+
+            return;
+        }
+
+        setNotificationList((prevNotiList) =>
+            prevNotiList.concat(
+                mapNotificationProps("AddExpense is failed", "danger")
+            )
+        );
+    };
+
+    const toMorePage = () => {
+        props.history.push("/more");
+    };
+
+    const onAddCategoryClickHandler = () => {
+        setIsShowAddCategory((prev) => !prev);
+    };
+
+    const addCategory = async (name: string) => {
+        const response = await CategoryService.addCategory({ name });
+
+        if (!response.isSuccess) {
+            setNotificationList((prevNotiList) =>
+                prevNotiList.concat(
+                    mapNotificationProps("Create category failed", "danger")
+                )
+            );
+
+            return;
+        }
+
+        const newCategories = categories.concat({ name });
+
+        setCategories(newCategories);
+        setNotificationList((prevNotiList) =>
+            prevNotiList.concat(
+                mapNotificationProps("Create category successful", "success")
+            )
+        );
+        setIsShowAddCategory(false);
+    };
+
+    const onNotificationRemove = (id: string) => {
+        setNotificationList((prevNotiList) =>
+            prevNotiList.filter((n) => n.id !== id)
+        );
+    };
+
     return isLoading ? (
         <Loading />
     ) : (
@@ -102,30 +212,26 @@ function Home2(props: RouteComponentProps) {
             <Toast
                 toastList={notificationList}
                 position="top-right"
-                onNotificationRemove={this.onNotificationRemove}
+                onNotificationRemove={onNotificationRemove}
             />
             <div className="content">
                 <div className="content__balance">
                     <span className="content__balance__text">
                         Balance:{" "}
-                        {this.state.currentValue.wallet
-                            ? this.state.currentValue.wallet.balance
-                            : 0}{" "}
+                        {currentValue.wallet ? currentValue.wallet.balance : 0}{" "}
                         THB
                     </span>
                     <Dropdown
                         className="content__balance__dropdown"
-                        options={this.state.wallets.map(
-                            (wallet) => wallet.name
-                        )}
-                        updateSelectedValue={this.updateSelectedWallet}
+                        options={wallets.map((wallet) => wallet.name)}
+                        updateSelectedValue={updateSelectedWallet}
                     />
                     <Link
                         className="content__balance__transaction__link"
                         to={{
                             pathname: `/transactionList/${
-                                this.state.currentValue.wallet
-                                    ? this.state.currentValue.wallet.name
+                                currentValue.wallet
+                                    ? currentValue.wallet.name
                                     : ""
                             }`,
                         }}
@@ -138,14 +244,14 @@ function Home2(props: RouteComponentProps) {
                     <DateBox
                         className="content__date__box"
                         name="date"
-                        updateValue={this.updateSelectedDate}
+                        updateValue={updateSelectedDate}
                     />
                 </div>
                 <div className="content__expense">
                     <span className="content__expense__text">Amount(THB) </span>
                     <TextBox
                         className="content__expense__box"
-                        updateValue={this.updateExpense}
+                        updateValue={updateExpense}
                         name="expnese"
                         type="number"
                     />
@@ -154,334 +260,31 @@ function Home2(props: RouteComponentProps) {
                     <span className="content__category__text">Category </span>
                     <Dropdown
                         className="content__category__dropdown"
-                        options={this.state.categories.map(
-                            (category) => category.name
-                        )}
-                        updateSelectedValue={this.updateSelectedCategory}
+                        options={categories.map((category) => category.name)}
+                        updateSelectedValue={updateSelectedCategory}
                     />
                     <Button
-                        onClickHandler={this.onAddCategoryClickHandler}
+                        onClickHandler={onAddCategoryClickHandler}
                         className="is-info is-light category__addBtn"
                         value="Create"
                     />
                 </div>
-                {this.renderAddCategory()}
+                {renderAddCategory()}
                 <div className="content__button">
                     <Button
                         className="content__button__add"
-                        onClickHandler={this.addTransaction}
+                        onClickHandler={addTransaction}
                         value="Add"
                     />
                     <Button
                         className="content__button__more"
-                        onClickHandler={this.toMorePage}
+                        onClickHandler={toMorePage}
                         value="More"
                     />
                 </div>
             </div>
         </Layout>
     );
-}
-
-class Home extends React.Component<RouteComponentProps, HomeState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            wallets: [],
-            categories: [],
-            currentValue: {
-                wallet: {
-                    name: "",
-                    balance: 0,
-                    type: "",
-                },
-                category: {
-                    name: "",
-                },
-                amount: 0,
-                date: moment().format("YYYY-MM-DD"),
-            },
-            isLoading: true,
-            isShowAddCategory: false,
-            notificationList: [],
-        };
-    }
-
-    public componentDidMount() {
-        Promise.all([this.fetchAllWallet(), this.fetchAllCategory()]).then(
-            () => {
-                this.setState({ isLoading: false });
-            }
-        );
-    }
-
-    public render() {
-        return this.state.isLoading ? (
-            <Loading />
-        ) : (
-            <Layout isShowBackwardIcon={false}>
-                <Toast
-                    toastList={this.state.notificationList}
-                    position="top-right"
-                    onNotificationRemove={this.onNotificationRemove}
-                />
-                <div className="content">
-                    <div className="content__balance">
-                        <span className="content__balance__text">
-                            Balance:{" "}
-                            {this.state.currentValue.wallet
-                                ? this.state.currentValue.wallet.balance
-                                : 0}{" "}
-                            THB
-                        </span>
-                        <Dropdown
-                            className="content__balance__dropdown"
-                            options={this.state.wallets.map(
-                                (wallet) => wallet.name
-                            )}
-                            updateSelectedValue={this.updateSelectedWallet}
-                        />
-                        <Link
-                            className="content__balance__transaction__link"
-                            to={{
-                                pathname: `/transactionList/${
-                                    this.state.currentValue.wallet
-                                        ? this.state.currentValue.wallet.name
-                                        : ""
-                                }`,
-                            }}
-                        >
-                            Transaction
-                        </Link>
-                    </div>
-                    <div className="content__date">
-                        <span className="content__date__text">Date </span>
-                        <DateBox
-                            className="content__date__box"
-                            name="date"
-                            updateValue={this.updateSelectedDate}
-                        />
-                    </div>
-                    <div className="content__expense">
-                        <span className="content__expense__text">
-                            Amount(THB){" "}
-                        </span>
-                        <TextBox
-                            className="content__expense__box"
-                            updateValue={this.updateExpense}
-                            name="expnese"
-                            type="number"
-                        />
-                    </div>
-                    <div className="content__category">
-                        <span className="content__category__text">
-                            Category{" "}
-                        </span>
-                        <Dropdown
-                            className="content__category__dropdown"
-                            options={this.state.categories.map(
-                                (category) => category.name
-                            )}
-                            updateSelectedValue={this.updateSelectedCategory}
-                        />
-                        <Button
-                            onClickHandler={this.onAddCategoryClickHandler}
-                            className="is-info is-light category__addBtn"
-                            value="Create"
-                        />
-                    </div>
-                    {this.renderAddCategory()}
-                    <div className="content__button">
-                        <Button
-                            className="content__button__add"
-                            onClickHandler={this.addTransaction}
-                            value="Add"
-                        />
-                        <Button
-                            className="content__button__more"
-                            onClickHandler={this.toMorePage}
-                            value="More"
-                        />
-                    </div>
-                </div>
-            </Layout>
-        );
-    }
-
-    private renderAddCategory = () => {
-        return this.state.isShowAddCategory ? (
-            <TextBoxWithButton onClick={this.addCategory} />
-        ) : null;
-    };
-
-    private updateSelectedDate = (value: string) => {
-        const currentValue = { ...this.state.currentValue };
-
-        currentValue.date = value;
-        this.setState({
-            currentValue,
-        });
-    };
-
-    private updateSelectedWallet = (value: string) => {
-        const currentValue = { ...this.state.currentValue };
-
-        currentValue.wallet = this.state.wallets.find((x) => x.name === value);
-        this.setState({
-            currentValue,
-        });
-    };
-
-    private updateSelectedCategory = (value: string) => {
-        const currentValue = { ...this.state.currentValue };
-
-        currentValue.category = this.state.categories.find(
-            (x) => x.name === value
-        );
-        this.setState({
-            currentValue,
-        });
-    };
-
-    private updateExpense = (value: string) => {
-        const currentValue = { ...this.state.currentValue };
-
-        currentValue.amount = Number.parseFloat(value);
-        this.setState({
-            currentValue,
-        });
-    };
-
-    private async fetchAllWallet() {
-        const wallets = await WalletService.getAllWallet();
-        const currentValue = { ...this.state.currentValue };
-
-        if (wallets?.length > 0) {
-            currentValue.wallet = wallets[0];
-        }
-
-        this.setState({
-            wallets: wallets ?? [],
-            currentValue,
-        });
-    }
-
-    private async fetchAllCategory() {
-        const categories = await CategoryService.getAllCategories();
-        const currentValue = { ...this.state.currentValue };
-
-        if (categories?.length > 0) {
-            currentValue.category = categories[0];
-        }
-
-        this.setState({
-            categories: categories ?? [],
-            currentValue,
-        });
-    }
-
-    private addTransaction = async () => {
-        const { wallet, amount, date, category } = this.state.currentValue;
-        const request: AddExpenseRequest = {
-            amount,
-            category: category ? category.name : "",
-            from: wallet ? wallet.name : "",
-            description: "quick add appense",
-            date,
-        };
-
-        const response = await addExpense(request);
-
-        if (response) {
-            const wallets = [...this.state.wallets];
-            const selectedWalletIdx = wallets.findIndex(
-                (x) => x.name === response.srcWallet.name
-            );
-
-            wallets[selectedWalletIdx].balance = response.srcWallet.balance;
-            this.setState({
-                wallets,
-            });
-
-            this.setState((prevState) => {
-                return {
-                    ...prevState,
-                    notificationList: prevState.notificationList.concat(
-                        mapNotificationProps("AddExpense sucess", "success")
-                    ),
-                };
-            });
-
-            return;
-        }
-
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                notificationList: prevState.notificationList.concat(
-                    mapNotificationProps("AddExpense is failed", "danger")
-                ),
-            };
-        });
-    };
-
-    private toMorePage = () => {
-        this.props.history.push("/more", {
-            ...this.state,
-        });
-    };
-
-    private onAddCategoryClickHandler = () => {
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                isShowAddCategory: !prevState.isShowAddCategory,
-            };
-        });
-    };
-
-    private addCategory = async (name: string) => {
-        const response = await CategoryService.addCategory({ name });
-
-        if (!response.isSuccess) {
-            this.setState((prevState) => {
-                return {
-                    ...prevState,
-                    notificationList: prevState.notificationList.concat(
-                        mapNotificationProps("Create category failed", "danger")
-                    ),
-                };
-            });
-            return;
-        }
-
-        const newCategories = this.state.categories.concat({ name });
-
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                categories: newCategories,
-                notificationList: prevState.notificationList.concat(
-                    mapNotificationProps(
-                        "Create category successful",
-                        "success"
-                    )
-                ),
-                isShowAddCategory: false,
-            };
-        });
-    };
-
-    private onNotificationRemove = (id: string) => {
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                notificationList: prevState.notificationList.filter(
-                    (n) => n.id !== id
-                ),
-            };
-        });
-    };
 }
 
 const HomeWithAuthProtection = withAuthProtection()(Home);

99ab04a8c272e0c2f7114bfa9b7292707e80e22c
oat9002 <oat9002@hotmail.com>
2020-10-05 00:53:26 +0700

    :construction: Nav styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,8 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import React from "react";
 import { Link } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { walletsState } from "../common/shareState";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { totalWalletsBalanceState, walletsState } from "../common/shareState";
 import Wallet from "../service/model/Wallet";
 import Drawer from "./bases/Drawer";
 import Toast from "./bases/Toast";
@@ -16,6 +16,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = (props) => {
     const [wallets] = useRecoilState(walletsState);
+    const totalWalletsBalance = useRecoilValue(totalWalletsBalanceState);
     const renderBackIcon = () => {
         return !!props.isShowBackwardIcon ? (
             <div className="header__backIcon">
@@ -30,17 +31,30 @@ const Layout: React.FC<LayoutProps> = (props) => {
 
     const renderBurgerMenuContent = (wallets: Wallet[]) => {
         return (
-            <div className="container is-fluid">
+            <div className="container is-mobile is-fluid">
                 <aside className="menu">
                     <p className="menu-label">Wallets</p>
                     <ul className="menu-list">
                         {wallets.map((x) => (
                             <li key={x.name}>
-                                <a>
-                                    {x.name} {x.balance} baht
-                                </a>
+                                <div className="columns is-mobile">
+                                    <div className="column is-three-fifths">
+                                        {x.name}
+                                    </div>
+                                    <div className="column">{x.balance}</div>
+                                    <div className="column">THB</div>
+                                </div>
                             </li>
                         ))}
+                        <li>
+                            <div className="columns is-mobile">
+                                <div className="column is-three-fifths"></div>
+                                <div className="column">
+                                    {totalWalletsBalance}
+                                </div>
+                                <div className="column">THB</div>
+                            </div>
+                        </li>
                     </ul>
                     <p className="menu-label">Transactions</p>
                     <ul className="menu-list">
@@ -62,25 +76,30 @@ const Layout: React.FC<LayoutProps> = (props) => {
     return (
         <>
             <Toast position="top-right" />
-            <nav className="navbar is-transparent is-mobile">
-                {renderBackIcon()}
-                <div className="navbar-item">
-                    <Drawer>{renderBurgerMenuContent(wallets)}</Drawer>
-                </div>
-                <div className="navbar-menu">
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <button
-                                className="header__signout button is-link is-inverted is-small"
-                                onClick={() => firebase.auth().signOut()}
-                            >
-                                Sign out
-                            </button>
-                        </div>
+            <div className="container header">
+                <div className="columns is-mobile">
+                    {/* {renderBackIcon()} */}
+                    <div className="column is-3 mt-3 ml-1">
+                        <Drawer>{renderBurgerMenuContent(wallets)}</Drawer>
+                    </div>
+                    <div className="column is-6 mt-2">
+                        <span className="has-text-weight-bold is-size-4 has-text-dark">
+                            Expense ledger
+                        </span>
+                    </div>
+                    <div className="column is-3">
+                        <button
+                            className="header__signout button is-link is-outlined is-small mt-3 mr-3"
+                            onClick={() => firebase.auth().signOut()}
+                        >
+                            Sign out
+                        </button>
                     </div>
                 </div>
-            </nav>
-            <section className="container is-mobile">{props.children}</section>
+            </div>
+            <section className="container is-mobile mt-5">
+                {props.children}
+            </section>
             <footer className="footer">
                 <div className="content has-text-centered">
                     <a href="https://www.freepik.com/vectors/banner">

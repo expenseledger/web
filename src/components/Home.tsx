7712c59b338322d28b4f7445186e664847e8fc70
Kohpai <whcwhc78@gmail.com>
2020-10-23 21:37:37 +0200

    :alien: api-v2 in home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,23 +3,21 @@ import * as R from "ramda";
 import * as React from "react";
 import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 import { useRecoilState } from "recoil";
+import { gql, useMutation } from "@apollo/client";
+
 import {
     categoriesState,
     toastState,
     walletsState,
 } from "../common/shareState";
-import * as CategoryService from "../service/CategoryService";
 import { mapNotificationProps } from "../service/Mapper";
-import { AddExpenseRequest } from "../service/model/Requests";
-import { addExpense } from "../service/TransactionService";
 import { formatNumber } from "../service/Utils";
-import * as WalletService from "../service/WalletService";
 import Button from "./bases/Button";
 import DateBox from "./bases/DateBox";
 import Dropdown from "./bases/Dropdown";
 import Loading from "./bases/Loading";
 import TextBox from "./bases/TextBox";
-import TextBoxWithButton from "./bases/TextBoxWithButton";
+// import TextBoxWithButton from "./bases/TextBoxWithButton";
 import { withAuthProtection } from "./hoc/WithAuthProtection";
 import "./Home.scss";
 import Layout from "./Layout";
@@ -30,6 +28,75 @@ interface CurrentValue {
     amount: number;
     date: string;
 }
+
+const CURRENT_USER = gql`
+    mutation CurrentUser {
+        currentUser(input: {}) {
+            owner {
+                accounts(orderBy: [NAME_ASC]) {
+                    nodes {
+                        id
+                        name
+                        type
+                        balance
+                    }
+                }
+                categories(orderBy: [NAME_ASC]) {
+                    nodes {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    }
+`;
+
+// const ADD_CATEGORY = gql`
+//     mutation AddCategory($name: String!) {
+//         createCategory(input: { name: $name }) {
+//             category {
+//                 id
+//                 name
+//             }
+//         }
+//     }
+// `;
+
+const ADD_EXPENSE = gql`
+    mutation AddExpense(
+        $amount: Float!
+        $description: String!
+        $categoryId: Int!
+        $fromAccountId: Int!
+    ) {
+        spend(
+            input: {
+                amount: $amount
+                description: $description
+                categoryId: $categoryId
+                fromAccountId: $fromAccountId
+            }
+        ) {
+            transaction {
+                id
+                amount
+                description
+                type
+                date
+                categoryId
+                fromAccountId
+                toAccountId
+                fromAccount {
+                    id
+                    name
+                    type
+                    balance
+                }
+            }
+        }
+    }
+`;
 
 const Home: React.FC<RouteComponentProps> = (props) => {
     const [wallets, setWallets] = useRecoilState(walletsState);
@@ -42,24 +109,28 @@ const Home: React.FC<RouteComponentProps> = (props) => {
         date: moment().format("YYYY-MM-DD"),
     });
     const [isLoading, setIsLoading] = React.useState(true);
-    const [isShowAddCategory, setIsShowAddCategory] = React.useState(false);
+    // const [isShowAddCategory, setIsShowAddCategory] = React.useState(false);
+
+    const [currentUser] = useMutation(CURRENT_USER);
+    // const [addCategory] = useMutation(ADD_CATEGORY);
+    const [addExpense] = useMutation(ADD_EXPENSE);
 
     React.useEffect(() => {
-        Promise.all([
-            WalletService.getAllWallet(),
-            CategoryService.getAllCategories(),
-        ]).then((responses) => {
-            const [tWallets, tCategories] = responses;
-            const sortByNameCaseInsensitive = R.sortBy<any>(
-                R.compose(R.toLower, R.prop("name"))
-            );
+        const main = async () => {
+            const { data, errors } = await currentUser();
 
-            setCategories(sortByNameCaseInsensitive(tCategories ?? []));
-            setWallets(sortByNameCaseInsensitive(tWallets ?? []));
+            if (errors) {
+                console.log("Kohpai-error", errors);
+                return;
+            }
+
+            setCategories(data.currentUser.owner.accounts.nodes);
+            setWallets(data.currentUser.owner.categories.nodes);
             setIsLoading(false);
-        });
+        };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        main();
+    }, [currentUser, setCategories, setWallets, setIsLoading]);
 
     const updateSelectedDate = (value: string) => {
         const tCurrentValue = R.clone(currentValue);
@@ -92,7 +163,7 @@ const Home: React.FC<RouteComponentProps> = (props) => {
     };
 
     const addTransaction = async () => {
-        const { walletIdx, amount, date, categoryIdx } = currentValue;
+        const { walletIdx, amount, categoryIdx } = currentValue;
 
         if (amount === 0) {
             setNotificationList((prev) =>
@@ -106,23 +177,23 @@ const Home: React.FC<RouteComponentProps> = (props) => {
             return;
         }
 
-        const request: AddExpenseRequest = {
-            amount,
-            category: categories[categoryIdx]?.name ?? "",
-            from: wallets[walletIdx]?.name ?? "",
-            description: "quick add appense",
-            date,
-        };
+        const { data, errors } = await addExpense({
+            variables: {
+                amount,
+                description: "quick add appense",
+                categoryId: categories[categoryIdx]?.id ?? 0,
+                fromAccountId: wallets[walletIdx]?.id ?? 0,
+            },
+        });
 
-        const response = await addExpense(request);
-
-        if (response) {
+        if (!errors) {
             const tWallets = R.clone(wallets);
+            const fromAccount = data.spend.transaction.fromAccount;
             const selectedWalletIdx = wallets.findIndex(
-                (x) => x.name === response.srcWallet.name
+                (x) => x.id === fromAccount.id
             );
 
-            tWallets[selectedWalletIdx].balance = response.srcWallet.balance;
+            tWallets[selectedWalletIdx].balance = fromAccount.balance;
             setWallets(tWallets);
             setNotificationList((prev) =>
                 prev.concat(
@@ -142,39 +213,37 @@ const Home: React.FC<RouteComponentProps> = (props) => {
         props.history.push("/more", { ...currentValue });
     };
 
-    const onAddCategoryClickHandler = () => {
-        setIsShowAddCategory((prev) => !prev);
-    };
+    // const onAddCategoryClickHandler = () => {
+    //     setIsShowAddCategory((prev) => !prev);
+    // };
 
-    const addCategory = async (name: string) => {
-        const response = await CategoryService.addCategory({ name });
+    // const onAddCategory = async (name: string) => {
+    //     const { data, errors } = await addCategory({
+    //         variables: { name },
+    //     });
+    //     if (errors) {
+    //         setNotificationList((prevNotiList) =>
+    //             prevNotiList.concat(
+    //                 mapNotificationProps("Create category failed", "danger")
+    //             )
+    //         );
+    //         return;
+    //     }
+    //
+    //     setCategories(categories.concat(data.createCategory.category));
+    //     setNotificationList((prevNotiList) =>
+    //         prevNotiList.concat(
+    //             mapNotificationProps("Create category successful", "success")
+    //         )
+    //     );
+    //     setIsShowAddCategory(false);
+    // };
 
-        if (!response.isSuccess) {
-            setNotificationList((prevNotiList) =>
-                prevNotiList.concat(
-                    mapNotificationProps("Create category failed", "danger")
-                )
-            );
-
-            return;
-        }
-
-        const newCategories = categories.concat({ name });
-
-        setCategories(newCategories);
-        setNotificationList((prevNotiList) =>
-            prevNotiList.concat(
-                mapNotificationProps("Create category successful", "success")
-            )
-        );
-        setIsShowAddCategory(false);
-    };
-
-    const renderAddCategory = () => {
-        return isShowAddCategory ? (
-            <TextBoxWithButton onClick={addCategory} />
-        ) : null;
-    };
+    // const renderAddCategory = () => {
+    //     return isShowAddCategory ? (
+    //         <TextBoxWithButton onClick={onAddCategory} />
+    //     ) : null;
+    // };
 
     return isLoading ? (
         <Loading />

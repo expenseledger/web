6a1136d5459d4ac75ad1fb87b17e1af89a6515dc
Kohpai <whcwhc78@gmail.com>
2020-10-23 13:57:03 +0200

    :sparkles: add apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,20 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bulma/css/bulma.css";
-import firebase from "firebase/app";
-import "firebase/auth";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
+import { ApolloProvider } from "@apollo/client";
+
 import App from "./container/App";
 import "./index.scss";
 import registerServiceWorker from "./registerServiceWorker";
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
+import apolloClient from "./lib/apollo";
 
 ReactDOM.render(
     <RecoilRoot>
-        <App />
+        <ApolloProvider client={apolloClient}>
+            <App />
+        </ApolloProvider>
     </RecoilRoot>,
     document.getElementById("root") as HTMLElement
 );

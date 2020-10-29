e007a74a29cfde3825e9c983bb853b472e099286
oat9002 <oat9002@hotmail.com>
2020-10-29 23:09:52 +0700

    :construction:

--- src/components/AddOrRemove.tsx
+++ src/components/AddOrRemove.tsx
@@ -9,35 +9,97 @@ export interface Item {
 export interface AddOrRemoveProps {
     addFunc?: (id: number) => Promise<void>;
     removeFunc?: (id: number) => Promise<void>;
-    currentItems?: Item[];
+    currentItems?: () => Item[];
 }
 
-const AddOrRemove: React.FC<AddOrRemoveProps> = () => {
-    const i = Array.from(Array(100).keys());
-    const renderItemList = () => (
-        <nav
-            className="panel"
-            style={{
-                overflowY: "scroll",
-                maxHeight: "80vh",
-                backgroundColor: "white",
-            }}
-        >
-            {i.map((i) => (
-                <a className="panel-block is-active is-primary" key={i}>
-                    <span className="panel-icon">
-                        <i className="fas fa-book" aria-hidden="true"></i>
-                    </span>
-                    bulma
-                </a>
-            ))}
-        </nav>
+interface ItemBoxProps {
+    removeFunc: (id: number) => Promise<void>;
+}
+
+const ItemBox: React.FC<ItemBoxProps> = (props) => {
+    const [isClickedDelete, setIsClickedDelete] = React.useState(false);
+    const onDeleteHandler = () => {
+        setIsClickedDelete(true);
+    };
+    const onCancelHandler = () => {
+        setIsClickedDelete(false);
+    };
+    const onConfirmHandler = async () => {
+        if (!props.removeFunc) {
+            return;
+        }
+
+        await props.removeFunc(1);
+        setIsClickedDelete(false);
+    };
+
+    const renderDelete = () => {
+        if (isClickedDelete) {
+            return (
+                <div className="columns">
+                    <div className="column">
+                        <button
+                            onClick={onCancelHandler}
+                            className="button is-primary is-small is-outlined"
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                    <div className="column">
+                        <button
+                            onClick={onConfirmHandler}
+                            className="button is-danger is-small is-primary"
+                        >
+                            Confirm
+                        </button>
+                    </div>
+                </div>
+            );
+        }
+
+        return (
+            <a onClick={onDeleteHandler}>
+                <span className="icon">
+                    <i
+                        className="fas fa-lg fa-times has-text-danger"
+                        aria-hidden="true"
+                    ></i>
+                </span>
+            </a>
+        );
+    };
+
+    return (
+        <a className="panel-block is-active is-primary">
+            <span className="panel-icon">
+                <i className="fas fa-book" aria-hidden="true"></i>
+            </span>
+            bulma
+            {renderDelete()}
+        </a>
     );
+};
+
+const AddOrRemove: React.FC<AddOrRemoveProps> = (props) => {
+    const i = Array.from(Array(100).keys());
 
     return (
         <Layout>
             <div className="columns is-mobile is-centered is-multiline">
-                <div className="column is-full">{renderItemList()}</div>
+                <div className="column is-full">
+                    <nav
+                        className="panel"
+                        style={{
+                            overflowY: "scroll",
+                            maxHeight: "80vh",
+                            backgroundColor: "white",
+                        }}
+                    >
+                        {i.map((i) => (
+                            <ItemBox removeFunc={props.removeFunc} key={i} />
+                        ))}
+                    </nav>
+                </div>
                 <div className="columns is-gapless is-centered is-mobile">
                     <div className="column is-four-fifths control">
                         <input

369c3e8dbbd4d0053a356ab3884736d89d4c7faa
oat9002 <oat9002@hotmail.com>
2020-11-24 00:24:22 +0700

    :contruction:

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { useRecoilState } from "recoil";
+import styled from "styled-components";
 import { toastState } from "../common/shareState";
 import Loading from "../components/bases/Loading";
-import { withAuthProtection } from "../components/hoc/WithAuthProtection";
 import Layout from "../components/Layout";
 import { TransactionCard } from "../components/TransactionCard";
 import { mapNotificationProps } from "../service/Mapper";
@@ -12,6 +12,7 @@ import {
     deleteTransaction,
     listTransactions,
 } from "../service/TransactionService";
+import { withAuthProtection } from "./hoc/WithAuthProtection";
 import "./TransactionList.scss";
 
 interface TransactionListProps extends RouteComponentProps {
@@ -65,25 +66,160 @@ export const TransactionList: React.FC<TransactionListProps> = (props) => {
     const cards = transactions.map((tx) => {
         const { id, date, amount, type, category, description } = tx;
         return (
-            <>
-                <TransactionCard
-                    date={date}
-                    amount={amount}
-                    type={type}
-                    category={category}
-                    description={description}
-                    key={id}
-                    onDelete={() => removeTransaction(id)}
-                />
-            </>
+            <TransactionCard
+                date={date}
+                amount={amount}
+                type={type}
+                category={category}
+                description={description}
+                key={id}
+                onDelete={() => removeTransaction(id)}
+            />
         );
     });
+
+    const MultilineDiv = styled.div`
+        word-wrap: break-word;
+    `;
 
     return isLoading ? (
         <Loading />
     ) : (
         <Layout>
-            <div>{cards}</div>
+            {/* <div>{cards}</div> */}
+            <div className="box">
+                <h1 className="title is-4">25 Nov 2020</h1>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <div className="columns is-mobile is-gapless is-multiline">
+                            <div className="column is-half">
+                                <span className="has-text-weight-bold">
+                                    Amount:
+                                </span>
+                            </div>
+                            <div className="column is-half">123</div>
+                            <div className="column is-half">
+                                <span className="has-text-weight-bold">
+                                    Category:
+                                </span>
+                            </div>
+                            <div className="column is-half">Good</div>
+                            <div className="column is-half">
+                                <span className="has-text-weight-bold">
+                                    Description:
+                                </span>
+                            </div>
+                            <div className="column is-half">
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industrys standard dummy text ever since the
+                                1500s,
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className="message">
+                    <div className="message-body">
+                        <p>
+                            <strong>Amount:</strong> 123
+                        </p>
+                        <p>
+                            <strong>Category:</strong> Food
+                        </p>
+                        <p>
+                            <strong>Description:</strong> expasdlkfjlaksdjf
+                        </p>
+                    </div>
+                </article>
+                <article className="message is-danger">
+                    <div className="message-body">
+                        <p>
+                            <strong>Amount:</strong> 123
+                        </p>
+                        <p>
+                            <strong>Category:</strong> Food
+                        </p>
+                        <p>
+                            <strong>Description:</strong> expasdlkfjlaksdjf
+                        </p>
+                    </div>
+                </article>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>
+                            <strong>Amount:</strong> 123
+                        </p>
+                        <p>
+                            <strong>Category:</strong> Food
+                        </p>
+                        <p>
+                            <strong>Description:</strong> expasdlkfjlaksdjf
+                        </p>
+                    </div>
+                </article>
+            </div>
+            <div className="box">
+                <h1 className="title is-4">25 Nov 2020</h1>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+                <article className="message is-danger">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+            </div>
+            <div className="box">
+                <h1 className="title is-4">25 Nov 2020</h1>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+                <article className="message is-danger">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+                <article className="message is-success">
+                    <div className="message-body">
+                        <p>Amount: 123</p>
+                        <p>Category: Food</p>
+                        <p>Description: expasdlkfjlaksdjf</p>
+                    </div>
+                </article>
+            </div>
         </Layout>
     );
 };

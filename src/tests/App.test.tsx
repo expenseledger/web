b05d0e9de3c9390f99eef46e60f872ce7a94cb17
oat9002 <oat9002@hotmail.com>
2022-01-09 00:36:29 +0700

    :bug: update test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,14 @@
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import React from "react";
+import App from "../components/App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// describe("<App>", () => {
-//     it("renders without crashing", () => {
-//         const wrapper = shallow(<App />);
-//         expect(toJson(wrapper)).toMatchSnapshot();
-//     });
-// });
+describe("<App>", () => {
+    it("renders without crashing", () => {
+        const wrapper = shallow(<App />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});

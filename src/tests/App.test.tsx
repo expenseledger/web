a0f46fb9da93fbf35201f66805e5ce76ba0a8785
oat9002 <oat9002@hotmail.com>
2022-01-09 00:31:11 +0700

    :recycle: remove test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,14 +1,11 @@
-import Enzyme, { shallow } from "enzyme";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import toJson from "enzyme-to-json";
-import React from "react";
-import App from "../components/App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("<App>", () => {
-    it("renders without crashing", () => {
-        const wrapper = shallow(<App />);
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
-});
+// describe("<App>", () => {
+//     it("renders without crashing", () => {
+//         const wrapper = shallow(<App />);
+//         expect(toJson(wrapper)).toMatchSnapshot();
+//     });
+// });

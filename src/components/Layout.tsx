c187174a12dca8c680a3e96edb5faca1241b81d4
oat9002 <oat9002@hotmail.com>
2022-06-17 23:44:31 +0700

    :sparkles: add version in hambuger

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
+import styled from "styled-components";
+import pj from "../../package.json";
 import Logo from "../assets/pics/logo.svg";
 import { accountsState, categoriesState, totalAccountsBalanceState } from "../common/shareState";
 import { formatNumber, log } from "../common/utils";
@@ -13,6 +15,14 @@ import Toast from "./bases/Toast";
 import { useSignIn } from "./hoc/WithAuthProtection";
 import "./Layout.scss";
 
+const Version = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    font-size: 0.5em;
+    margin-right: 5px;
+`;
+
 const Layout: React.FC = () => {
     const [accounts, setAccounts] = useRecoilState(accountsState);
     const [, setCategories] = useRecoilState(categoriesState);
@@ -22,54 +32,58 @@ const Layout: React.FC = () => {
 
     const renderBurgerMenuContent = (accounts: Account[]) => {
         return (
-            <div className="container is-mobile is-fluid mt-5">
-                <aside className="menu">
-                    <p className="menu-label">Accounts</p>
-                    <ul className="menu-list">
-                        {accounts.map((x) => (
-                            <li key={x.name}>
+            <>
+                <div className="container is-mobile is-fluid mt-5">
+                    <aside className="menu">
+                        <p className="menu-label">Accounts</p>
+                        <ul className="menu-list">
+                            {accounts.map((x) => (
+                                <li key={x.name}>
+                                    <div className="columns is-mobile">
+                                        <div className="column is-half">{x.name}</div>
+                                        <div className="column">฿ {formatNumber(x.balance)}</div>
+                                    </div>
+                                </li>
+                            ))}
+                            <li>
                                 <div className="columns is-mobile">
-                                    <div className="column is-half">{x.name}</div>
-                                    <div className="column">฿ {formatNumber(x.balance)}</div>
+                                    <div className="column is-half menu__totalBalance">=</div>
+                                    <div className="column has-text-weight-bold">
+                                        ฿ {formatNumber(totalAccountsBalance)}
+                                    </div>
                                 </div>
                             </li>
-                        ))}
-                        <li>
-                            <div className="columns is-mobile">
-                                <div className="column is-half menu__totalBalance">=</div>
-                                <div className="column has-text-weight-bold">
-                                    ฿ {formatNumber(totalAccountsBalance)}
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                    <p className="menu-label">Setting</p>
-                    <ul className="menu-list">
-                        <li>
-                            <ul>
-                                <li>
-                                    <Link to="/account/setting">Account</Link>
-                                </li>
-                                <li>
-                                    <Link to="/category/setting">Category</Link>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <p className="menu-label">Misc</p>
-                    <ul className="menu-list">
-                        <li>
-                            <ul>
-                                <a onClick={() => auth.signOut()}>Sign out</a>
-                            </ul>
-                        </li>
-                    </ul>
-                </aside>
-            </div>
+                        </ul>
+                        <p className="menu-label">Setting</p>
+                        <ul className="menu-list">
+                            <li>
+                                <ul>
+                                    <li>
+                                        <Link to="/account/setting">Account</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/category/setting">Category</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <p className="menu-label">Misc</p>
+                        <ul className="menu-list">
+                            <li>
+                                <ul>
+                                    <a onClick={() => auth.signOut()}>Sign out</a>
+                                </ul>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+                <Version>v{pj.version}</Version>
+            </>
         );
     };
 
     React.useEffect(() => {
+        console.log(process.env);
         if (!isSignInLoading && !isSignIn) {
             redirectToSignIn();
             return;

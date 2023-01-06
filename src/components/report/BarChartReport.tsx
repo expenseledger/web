50f52f01466974abf77b7480788ec55b534165b6
oat9002 <oat9002@hotmail.com>
2023-01-07 01:26:27 +0700

    :construction:

--- src/components/report/BarChartReport.tsx
+++ src/components/report/BarChartReport.tsx
@@ -1,7 +1,16 @@
 import dayjs from "dayjs";
 import * as R from "ramda";
 import { useEffect, useState } from "react";
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Legend,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from "recharts";
 import Transaction from "../../service/model/Transaction";
 
 export interface BarChartReportProps {
@@ -20,9 +29,9 @@ const BarChartReport: React.FC<BarChartReportProps> = (props) => {
         const toReturn: BarChartData[] = [];
         const byName = R.groupBy((data: BarChartData) => data.name);
         const dirtyData = byName(
-            transactions.map((t) => {
+            transactions.reverse().map((t) => {
                 return {
-                    name: dayjs(t.date).format("DD/MM/YYYY"),
+                    name: dayjs(t.date).format("D MMM"),
                     income: t.type === "INCOME" ? Math.abs(t.amount) : 0,
                     expense: t.type !== "INCOME" ? Math.abs(t.amount) : 0,
                 };
@@ -51,24 +60,26 @@ const BarChartReport: React.FC<BarChartReportProps> = (props) => {
     }, [props.transactions]);
 
     return !data ? null : (
-        <BarChart
-            width={1000}
-            height={500}
-            data={data}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="income" fill="#8884d8" />
-            <Bar dataKey="expense" fill="#82ca9d" />
-        </BarChart>
+        <div style={{ width: "100%", height: 200 }}>
+            <ResponsiveContainer>
+                <BarChart
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="income" fill="#8884d8" />
+                    <Bar dataKey="expense" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     );
 };
 

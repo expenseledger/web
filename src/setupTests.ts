12abf80de523cb69e41ccbd5997ea3779e8b76aa
oat9002 <oat9002@hotmail.com>
2023-02-12 01:13:05 +0700

    :construction: config jest

---
+++ src/setupTests.ts
@@ -0,0 +1,4 @@
+import React from "react";
+
+// This line is important for avoiding the "ReferenceError: React is not defined" error
+global.React = React;
--- src/tests/AccountCard.test.tsx
+++ src/tests/AccountCard.test.tsx
@@ -1,13 +1,11 @@
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import toJson from "enzyme-to-json";
+import renderer from "react-test-renderer";
 import AccountCard from "../components/AccountCard";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe("<AccountCard>", () => {
     it("renders without crashing", () => {
-        const wrapper = shallow(<AccountCard id={1} balance={12.34} name="test" currency="฿" />);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const component = renderer.create(
+            <AccountCard id={1} balance={12.34} name="test" currency="฿" />
+        );
+        expect(component.toJSON()).toMatchSnapshot();
     });
 });

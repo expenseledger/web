04bf97659ff22ac3abd827534d73d9fe32645eb7
oat9002 <oat9002@hotmail.com>
2023-02-12 10:12:32 +0700

    :recycle: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgrPlugin()],
+    plugins: [
+        react(),
+        svgrPlugin(),
+        VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+    ],
     build: {
         outDir: "build",
     },

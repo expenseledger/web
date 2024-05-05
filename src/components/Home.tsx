afe6eda4619434af58df2d5ec50e9553f008cf01
oat9002 <oat9002@hotmail.com>
2024-05-05 16:05:49 +0700

    💄

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -31,9 +31,6 @@ interface CurrentValue {
 const LinkText = styled.span`
     vertical-align: middle;
 `;
-const InputBox = styled(Box)`
-    grid-column: 2 / span 2;
-`;
 
 const Home: React.FC = () => {
     const initialState: CurrentValue = {
@@ -199,51 +196,59 @@ const Home: React.FC = () => {
                     </Link>
                 </Text>
             </Flex>
-            <Grid columns="3" gap="4" align="center">
-                <Box>
-                    <Text weight="bold">Amount</Text>
-                </Box>
-                <InputBox>
-                    <TextBox
-                        updateValue={updateExpense}
-                        name="expense"
-                        type="number"
-                        value={currentValue.amount}
-                        addOn={{ text: currency, position: "front" }}
-                    />
-                </InputBox>
+            <Grid rows="3" gap="3" align="center">
+                <Flex direction="column">
+                    <Box>
+                        <Text weight="bold">Amount</Text>
+                    </Box>
+                    <Box>
+                        <TextBox
+                            updateValue={updateExpense}
+                            name="expense"
+                            type="number"
+                            value={currentValue.amount}
+                            addOn={{ text: currency, position: "front" }}
+                        />
+                    </Box>
+                </Flex>
+                <Flex direction="column">
                 <Box>
                     <Text weight="bold">Date</Text>
                 </Box>
-                <InputBox>
+                <Box>
                     <DateBox
                         name="date"
                         updateValue={updateSelectedDate}
                         value={currentValue.date}
                     />
-                </InputBox>
+                </Box>
+                </Flex>
+                <Flex direction="column">
                 <Box>
                     <Text weight="bold">Category</Text>
                 </Box>
-                <InputBox>
+                <Box>
                     <Dropdown
                         options={categories
                             .filter((c) => c.type === "ANY" || c.type === "EXPENSE")
                             .map((c) => c.name)}
                         updateSelectedValue={updateSelectedCategory}
                     />
-                </InputBox>
+                </Box>
+                </Flex>
+                <Flex direction="column">
                 <Box>
                     <Text weight="bold">Description</Text>
                 </Box>
-                <InputBox>
+                <Box>
                     <TextBox
                         name="description"
                         updateValue={updateDescription}
                         value={currentValue.description}
                         placeholder="Optional"
                     />
-                </InputBox>
+                </Box>
+                </Flex>
                 <Flex mt="2">
                     <Box>
                         <Button

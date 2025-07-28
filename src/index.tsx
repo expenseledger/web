a0a25fb220ce82c4e9fd287fbe17c62e72f95635
oat9002 <oat9002@hotmail.com>
2025-07-28 15:48:46 +0700

    refactor: optimize Swiper imports and update state management in Home component

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
 import { createRoot } from "react-dom/client";
-import SwiperCore from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import App from "./components/App";
 import "@radix-ui/themes/styles.css";
 import "./index.scss";
 import apolloClient from "./lib/apollo";
 import { Theme } from "@radix-ui/themes";
 import { color } from "./common/constants";
-
-SwiperCore.use([Pagination, Navigation]);
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);

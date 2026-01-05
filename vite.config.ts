5c923d91a702d34eb392110cbd16a7440b7238f1
oat9002 <oat9002@hotmail.com>
2026-01-05 20:48:43 +0700

    Update Babel and Vite configuration for Linaria integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import svgrPlugin from "vite-plugin-svgr";
+import wyw from "@wyw-in-js/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -40,6 +41,7 @@ export default defineConfig({
                 display: "standalone",
             },
         }),
+        wyw(),
     ],
     build: {
         outDir: "build",

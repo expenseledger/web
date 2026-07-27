ead495d4ea9da3b9ca2d33bd4856a665e9aa2218
oat9002 <oat9002@hotmail.com>
2026-07-27 10:54:07 +0700

    fix

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,9 +4,9 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // This line is important for avoiding the "ReferenceError: React is not defined" error
-global.React = React;
+globalThis.React = React;
 
-global.ResizeObserver = class ResizeObserver {
+globalThis.ResizeObserver = class ResizeObserver {
     observe() {}
     unobserve() {}
     disconnect() {}
